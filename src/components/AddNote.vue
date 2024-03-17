3791566a4ad6430dced30d873a70cfd5c8197bbd
Elvis <elvisptavares@gmail.com>
2024-03-17 16:35:47 -0300

    created component create a new note

---
+++ src/components/AddNote.vue
@@ -0,0 +1,30 @@
+<template>
+    <form action="" @submit.prevent="addNote">
+        <input type="text" v-model="title" placeholder="Titulo">
+        <textarea v-model="content"  placeholder="Conteúdo" cols="30" rows="10"></textarea>
+        <button type="submit">Adicionar nota</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: '',
+                content: '',
+            };
+        },
+        methods : {
+            addNote() {
+                const note = {
+                    title: this.title,
+                    content: this.content,
+                };
+
+                this.$emit('new-note', note);
+                this.title = '';
+                this.content = '';
+            }
+        }
+    };
+</script>
